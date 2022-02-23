512479f7608214d5ede6029d38d529eb8ca9efc0
akarelia20 <97579245+akarelia20@users.noreply.github.com>
2022-02-22 23:20:01 -0800

    button clicker assignment (#12)
    Authored-by: Amee Karelia

---
+++ week3/button_clicker/script.js
@@ -0,0 +1,12 @@
+function changeText(element) {
+    console.log('hello');
+    element.innerText="Logout";
+}
+
+function remove(element) {
+    element.remove();
+}
+
+function message() {
+    alert("Ninja was liked");
+}
